2d85fb52da09cf1f57bc713e795899c54f9886ae
Azfa025 <azfaherm@gmail.com>
2022-08-23 19:17:37 +0700

    pertemuan 3 dan soal 1

---
+++ ITechno_49_Python_Basic/pertemuan-03.py
@@ -0,0 +1,21 @@
+#if, elif, else
+
+x = 20
+y = 6
+z = 20
+if x < y:
+    
+    print("benar nilai x lebih besar dari y")
+    
+elif x != z and x > y :
+    
+    print("benar nilai x sama dengan z dan x lebih besar dari y")
+    
+else:
+    print("salah")
+
+
+print("diluar if, elif, else")
+
+
+
--- ITechno_49_Python_Basic/soal-01_tahun_kabisat.py
+++ ITechno_49_Python_Basic/soal-01_tahun_kabisat.py
@@ -1,13 +1,25 @@
-'''
-membuat variable untuk meminta input pada user
 
+#Tahun kabisat
 
+#membuat variable untuk meminta input pada user
 
-Jika habis dibagi 400, tahun kabisat
-Jika tidak habis dibagi 400, tetapi habis dibagi 100, bukan tahun kabisat
-Jika tiak habis dibagi 400, tidak habis dibagi 100, tapi habis dibagi 4, tahun kabisat
-Jika tidak habis dibagi 400, tidak habis dibgai 100, tidak habis dibagi 4, bukan tahun kabisat
-'''
+#Jika habis dibagi 400, tahun kabisat
+#Jika tidak habis dibagi 400, tetapi habis dibagi 100, bukan tahun kabisat
+#Jika tidak habis dibagi 400, tidak habis dibagi 100, tapi habis dibagi 4, tahun kabisat
+#Jika tidak habis dibagi 400, tidak habis dibgai 100, tidak habis dibagi 4, bukan tahun kabisat
 
+tahun = int(input())
 
+if tahun % 400 == 0:
+    print("tahun kabisat")
+
+elif tahun % 400 != 0 and tahun % 100 == 0 :
+    print("bukan tahun kabisat")
+
+elif tahun % 400 != 0 and tahun % 100 != 0 and tahun % 4 == 0:
+    print("tahun Kabisat")
+   
+
+else:
+    print("bukan tahun kabisat")
 
