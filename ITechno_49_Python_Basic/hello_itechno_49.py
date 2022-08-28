9856959ac8dbf24ab85b5fdb02a258d8a19d7525
Azfa025 <azfaherm@gmail.com>
2022-08-29 06:05:50 +0700

    pertemuan ke -3 dan soal tahun kabisat

--- ITechno_49_Python_Basic/hello_itechno_49.py
+++ ITechno_49_Python_Basic/hello_itechno_49.py
@@ -1,1 +1,10 @@
-print("Hello ITechno 49")
+a = 10
+b = 20
+
+tambah = a + b
+kurang = a - b
+kali = a * b
+bagi = a / b
+eksponen = a ** 4
+floordivision = b // 4
+modulo = a % 4
---
+++ ITechno_49_Python_Basic/helloworld.cpp
@@ -0,0 +1,6 @@
+#Include <iostream>
+
+int main{}(
+    std::cout << "Hello World" << std::endl;
+    return 0
+)
